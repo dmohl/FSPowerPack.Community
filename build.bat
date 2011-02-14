108e0a662eff423d4ce26882aaac96eeacc4d765
Dan Mohl <dmohl@yahoo.com>
2011-02-14 13:15:23 -0600

    Created

---
+++ build.bat
@@ -0,0 +1,5 @@
+c:\nuget\nuget.exe pack c:\git\FSPowerPack.Community\FSPowerPack.Community.nuspec -b c:\git\FSPowerPack.Community -o c:\git\FSPowerPack.Community
+
+md c:\nuget\FSPowerPack.Community\
+copy c:\git\FSPowerPack.Community\*.nupkg c:\nuget\FSPowerPack.Community /Y
+pause
