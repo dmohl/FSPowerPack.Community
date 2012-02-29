bc404518ff56bfccfe71b85fe6b071bb0e0cecb6
Terence Lewis <lewistm@gmail.com>
2012-02-29 17:35:24 +0200

    Updated commands to resolve parameter ambiguity warnings with Nuget 1.6

--- FSPowerPack.Community.Sample/build.bat
+++ FSPowerPack.Community.Sample/build.bat
@@ -1,4 +1,4 @@
-c:\nuget\nuget.exe pack c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample\FSPowerPack.Community.Sample.nuspec -b c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample -o c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample
+c:\nuget\nuget.exe pack c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample\FSPowerPack.Community.Sample.nuspec -BasePath c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample -o c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample
 
 md c:\nuget\FSPowerPack.Community.Sample\
 copy c:\git\FSPowerPack.Community\FSPowerPack.Community.Sample\*.nupkg c:\nuget\FSPowerPack.Community.Sample /Y
--- FSPowerPack.Parallel.Seq.Community/build.bat
+++ FSPowerPack.Parallel.Seq.Community/build.bat
@@ -1,4 +1,4 @@
-c:\nuget\nuget.exe pack c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community\FSPowerPack.Parallel.Seq.Community.nuspec -b c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community -o c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community
+c:\nuget\nuget.exe pack c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community\FSPowerPack.Parallel.Seq.Community.nuspec -BasePath c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community -o c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community
 
 md c:\nuget\FSPowerPack.Parallel.Seq.Community\
 copy c:\git\FSPowerPack.Community\FSPowerPack.Parallel.Seq.Community\*.nupkg c:\nuget\FSPowerPack.Parallel.Seq.Community /Y
